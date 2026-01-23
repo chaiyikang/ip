02fc2553961dfa9a437050caef2ede5bac5b0f14
Chai Yi Kang <chaiyikang52@gmail.com>
2026-01-23 16:32:32 +0800

    Implement Level-1:Echo

--- src/main/java/Steve.java
+++ src/main/java/Steve.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Steve {
     public static void main(String[] args) {
         System.out.println("Hello, I'm Steve!\nWhat can I help you with?");
+        String divider = "----------";
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String inpt = sc.nextLine();
+            if (inpt.equals("bye")) {
+                break;
+            }
+            System.out.println(divider);
+            System.out.println(inpt + "? uh...ok");
+            System.out.println(divider);
+        }
+
+
         System.out.println("---------------------");
         System.out.println("Bye! Glad I could help!");
     }

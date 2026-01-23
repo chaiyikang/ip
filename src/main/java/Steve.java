2c1be818fbde2ad260b13f54d565a70e1946064d
Chai Yi Kang <chaiyikang52@gmail.com>
2026-01-23 16:16:21 +0800

    Implement initial skeletal version
    This version simply greets the user and exits. It was decided that the
    bot be called Steve because Steve is a great name.

---
+++ src/main/java/Steve.java
@@ -0,0 +1,7 @@
+public class Steve {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Steve!\nWhat can I help you with?");
+        System.out.println("---------------------");
+        System.out.println("Bye! Glad I could help!");
+    }
+}

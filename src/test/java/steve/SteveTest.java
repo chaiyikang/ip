f835b8a1f4c0e5006886801f4777fd969426fee8
Chai Yi Kang <chaiyikang52@gmail.com>
2026-01-31 13:13:39 +0800

    Add dummy JUnit test

---
+++ src/test/java/steve/SteveTest.java
@@ -0,0 +1,15 @@
+package steve;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class SteveTest {
+    @Test
+    public void dummy() {
+        assertEquals(2, 2);
+    }
+
+    
+}
